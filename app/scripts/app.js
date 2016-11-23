5ba7c993b182da2cbe58416f128e6d9ee50998a8
18235608835 <798589050@qq.com>
2016-11-23 10:14:41 +0800

    1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,14 +9,6 @@
  * Main module of the application.
  */
 angular
-  .module('1512App', ['ui.router','fbb']).config(function($stateProvider,$urlRouterProvider){
-  	$stateProvider.state('list1',{
-  		url:"/list1",
-  		templateUrl:"../views/list1.html",
-  		controller:"list1"
-  	}).state('list2',{
-  		url:"/list2",
-  		templateUrl:"../views/list2.html",
-  		controller:"list2"
-  	})
-  })
+  .module('1512App', [])
+
+
